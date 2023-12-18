c0bb98dc8c49853545dd867144b656de46aeb249
dmitrimahayana <dmitri.mahayana@gmail.com>
2023-12-18 09:01:01 +0700

    update streamlit

---
+++ 03_Streamlit_Stable_Diff.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import requests
+import io
+from PIL import Image
+
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+
+
+def query_stabilitydiff(payload, headers):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
+
+
+with st.sidebar:
+    "[View the source code](https://github.com/dmitrimahayana/Py-LangChain-ChatGPT-VirtualAssistance/03_Streamlit_Stable_Diff.py)"
+
+st.title("💬 Chatbot - Text to Image")
+st.caption("🚀 A Streamlit chatbot powered by Stable Diffusion")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "What kind of image that I need to draw?"}]
+
+# Show previous prompts and results that saved in session
+for message in st.session_state.messages:
+    if message["role"] == "user":
+        st.chat_message("user").write(message["content"])
+    elif message["role"] == "assistant":
+        st.chat_message("assistant").write(message["content"])
+        if "image" in message:
+            st.chat_message("assistant").image(message["image"], caption=message["prompt"], use_column_width=True)
+
+if prompt := st.chat_input():
+
+    if not st.secrets.hugging_face_token.api_key:
+        st.info("Please add your Hugging Face Token to continue.")
+        st.stop()
+
+    # Input prompt
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    # Query Stable Diffusion
+    headers = {"Authorization": f"Bearer {st.secrets.hugging_face_token.api_key}"}
+    image_bytes = query_stabilitydiff({
+        "inputs": prompt,
+    }, headers)
+
+    # Return Image
+    image = Image.open(io.BytesIO(image_bytes))
+    msg = f'here is your image related to "{prompt}"'
+
+    # Show Result
+    st.session_state.messages.append({"role": "assistant", "content": msg, "prompt": prompt, "image": image})
+    st.chat_message("assistant").write(msg)
+    st.chat_message("assistant").image(image, caption=prompt, use_column_width=True)
